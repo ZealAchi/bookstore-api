a32a9ad3bc1f7b2ed061d5b5b62ec206b5364d83
Luis Antonio Padre Garcia <luispadre@protonmail.com>
2021-02-09 14:17:44 -0600

    Finalizando clases de dominio

--- src/main/java/com/padre/bookstore/domain/Categoria.java
+++ src/main/java/com/padre/bookstore/domain/Categoria.java
@@ -1,14 +1,27 @@
 package com.padre.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Categoria {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+@Entity
+public class Categoria implements Serializable{
+
+	private static final long serialVersionUID=1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private String description; 
 	
+	@OneToMany(mappedBy = "categoria")
 	private List<Libro>libros=new ArrayList<>();
 
 	public Categoria() {
--- src/main/java/com/padre/bookstore/domain/Libro.java
+++ src/main/java/com/padre/bookstore/domain/Libro.java
@@ -1,12 +1,28 @@
 package com.padre.bookstore.domain;
 
-public class Libro {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Libro  implements Serializable{
+
+	private static final long serialVersionUID=1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String title;
 	private String name_author;
 	private String text;
 	
+	@ManyToOne
+	@JoinColumn(name="categoria_id")
 	private Categoria categoria;
 	
 	
